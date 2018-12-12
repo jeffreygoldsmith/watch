04510c289c7fcf94dd2f42d55887275050a85c13
Jeffrey Goldsmith <jeff.goldsmith@shopify.com>
2018-12-12 14:04:36 -0500

    Added comments

--- TimeRows.cpp
+++ TimeRows.cpp
@@ -1,10 +1,21 @@
 #include "TimeRows.h"
 
+
+//
+// Class responsible for time row state storage.
+//
+
+//
+// TimeRows::TimeRows() -- Class constructor
+//
 TimeRows::TimeRows(Vector<Row> rows)
 {
   this->rows = rows;
 }
 
+//
+// TimeRows::isEditingModeEnabled() -- Iterate through rows to determine if isSelectedForEditing is enabled on any row.
+//
 bool TimeRows::isEditingModeEnabled()
 {
   for (int i = 0; i < rows.size(); i++)
@@ -17,6 +28,9 @@ bool TimeRows::isEditingModeEnabled()
 }
 
 
+//
+// TimeRows::deselectRows() -- Iterate through rows and ensure isSelectedForEditing flag cleared false on each row.
+//
 void TimeRows::deselectRows()
 {
   for (int i = 0; i < rows.size(); i++)
@@ -27,6 +41,9 @@ void TimeRows::deselectRows()
 }
 
 
+//
+// TimeRows::indexOfSelectedRow() -- Iterate through rows and determine the index of a selected row if one exists.
+//
 int TimeRows::indexOfSelectedRow()
 {
   for (int i = 0; i < rows.size(); i++)
